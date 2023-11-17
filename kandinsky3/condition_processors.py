d7612f5146da58afc46a83cf56652b56c13de716
oriBetelgeuse <arkhipkin.v98@gmail.com>
2023-11-17 19:43:23 +0300

    fix negative_prompt

--- kandinsky3/condition_processors.py
+++ kandinsky3/condition_processors.py
@@ -30,7 +30,7 @@ class T5TextConditionProcessor:
         self.tokens_length = tokens_length['t5']
         self.processor = T5Tokenizer.from_pretrained(processor_names['t5'])
 
-    def encode(self, text=None, image=None, ):
+    def encode(self, text=None, negative_text=None):
         encoded = self.processor(text, max_length=self.tokens_length, truncation=True)
         pad_length = self.tokens_length - len(encoded['input_ids'])
         input_ids = encoded['input_ids'] + [self.processor.pad_token_id] * pad_length
@@ -39,4 +39,17 @@ class T5TextConditionProcessor:
             'input_ids': torch.tensor(input_ids, dtype=torch.long),
             'attention_mask': torch.tensor(attention_mask, dtype=torch.long)
         }}
-        return condition_model_input
+
+        if negative_text is not None:
+            negative_encoded = self.processor(negative_text, max_length=self.tokens_length, truncation=True)
+            negative_input_ids = negative_encoded['input_ids'][:len(encoded['input_ids'])]
+            negative_input_ids[-1] = self.processor.eos_token_id
+            negative_input_ids = negative_input_ids + [self.processor.pad_token_id] * pad_length
+            negative_attention_mask = encoded['attention_mask'] + [0] * pad_length
+            negative_condition_model_input = {'t5': {
+                'input_ids': torch.tensor(negative_input_ids, dtype=torch.long),
+                'attention_mask': torch.tensor(negative_attention_mask, dtype=torch.long)
+            }}
+        else:
+            negative_condition_model_input = None
+        return condition_model_input, negative_condition_model_input
--- kandinsky3/t2i_pipeline.py
+++ kandinsky3/t2i_pipeline.py
@@ -47,7 +47,7 @@ class Kandinsky3T2IPipeline:
     def __call__(
         self, 
         text: str,
-        negative_text: str = '',
+        negative_text: str = None,
         images_num: int = 1,
         bs: int = 1, 
         width: int = 1024,
@@ -59,14 +59,13 @@ class Kandinsky3T2IPipeline:
         betas = get_named_beta_schedule('cosine', steps)
         base_diffusion = BaseDiffusion(betas, 0.99)
         
-        condition_model_input = self.t5_processor.encode(text)
+        condition_model_input, negative_condition_model_input = self.t5_processor.encode(text, negative_text)
         for key in condition_model_input:
             for input_type in condition_model_input[key]:
                 condition_model_input[key][input_type] = condition_model_input[key][input_type].unsqueeze(0).to(self.device)
 
-        if len(negative_text) > 0:
-            negative_condition_model_input = self.t5_processor.encode(negative_text)
-            for key in condition_model_input:
+        if negative_condition_model_input is not None:
+            for key in negative_condition_model_input:
                 for input_type in negative_condition_model_input[key]:
                     negative_condition_model_input[key][input_type] = negative_condition_model_input[key][input_type].unsqueeze(0).to(self.device)
         
@@ -74,12 +73,8 @@ class Kandinsky3T2IPipeline:
         with torch.cuda.amp.autocast(enabled=self.fp16):
             with torch.no_grad():
                 context, context_mask = self.t5_encoder(condition_model_input)
-
-                if len(negative_text) > 0:
-                    negative_context, negative_context_mask = torch.zeros_like(context), torch.zeros_like(context_mask)
-                    true_negative_context, true_negative_context_mask = self.t5_encoder(negative_condition_model_input)
-                    negative_context[:, :true_negative_context.shape[1]] = true_negative_context[:, :context.shape[1]]
-                    negative_context_mask[:, :true_negative_context.shape[1]] = true_negative_context_mask[:, :context.shape[1]]
+                if negative_condition_model_input is not None:
+                    negative_context, negative_context_mask = self.t5_encoder(negative_condition_model_input)
                 else:
                     negative_context, negative_context_mask = None, None
 
@@ -89,11 +84,16 @@ class Kandinsky3T2IPipeline:
                         continue
                     bs_context = repeat(context, '1 n d -> b n d', b=minibatch)
                     bs_context_mask = repeat(context_mask, '1 n -> b n', b=minibatch)
+                    if negative_context is not None:
+                        bs_negative_context = repeat(negative_context, '1 n d -> b n d', b=minibatch)
+                        bs_negative_context_mask = repeat(negative_context_mask, '1 n -> b n', b=minibatch)
+                    else:
+                        bs_negative_context, bs_negative_context_mask = None, None
 
                     images = base_diffusion.p_sample_loop(
                         self.unet, (minibatch, 4, height//8, width//8), self.device, 
                         bs_context, bs_context_mask, self.null_embedding, guidance_scale,
-                        negative_context=negative_context, negative_context_mask=negative_context_mask
+                        negative_context=bs_negative_context, negative_context_mask=bs_negative_context_mask
                     )
 
                     images = torch.cat([self.movq.decode(image) for image in images.chunk(2)])
