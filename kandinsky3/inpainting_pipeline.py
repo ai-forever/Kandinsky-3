ddc8f2100546ec743aa242ddfcef1a007a9323f7
Zein Shaheen <zeineshahine@gmail.com>
2023-11-19 19:58:18 +0300

    added batching + neg prompt inpainting

--- kandinsky3/inpainting_pipeline.py
+++ kandinsky3/inpainting_pipeline.py
@@ -49,6 +49,7 @@ class Kandinsky3InpaintingPipeline:
     def shared_step(self, batch: dict) -> dict:
         image = batch['image']
         condition_model_input = batch['text']
+        negative_condition_model_input = batch['negative_text']
         
         bs = image.shape[0]
         
@@ -66,9 +67,16 @@ class Kandinsky3InpaintingPipeline:
         mask = torch.nn.functional.interpolate(mask, size=(masked_latent.shape[2], masked_latent.shape[3]))
         context, context_mask = self.t5_encoder(condition_model_input)
         
+        if negative_condition_model_input is not None:
+            negative_context, negative_context_mask = self.t5_encoder(negative_condition_model_input)
+        else:
+            negative_context, negative_context_mask = None, None
+
         return {
             'context': context, 
             'context_mask': context_mask, 
+            'negative_context': negative_context,
+            'negative_context_mask': negative_context_mask,
             'image': image,
             'masked_latent': masked_latent,
             'mask': mask
@@ -77,14 +85,16 @@ class Kandinsky3InpaintingPipeline:
     def prepare_batch(
         self, 
         text: str, 
+        negative_text: str,
         image: PIL.Image.Image,
         mask: np.ndarray,
     ) -> dict:
-        condition_model_input, _ = self.t5_processor.encode(text=text)
+        condition_model_input, negative_condition_model_input = self.t5_processor.encode(text=text, negative_text=negative_text)
         batch = {
             'image': self.to_tensor(resize_image_for_diffusion(image.convert("RGB"))) * 2 - 1,
             'mask': 1 - self.to_tensor(resize_mask_for_diffusion(mask)),
-            'text' : condition_model_input
+            'text' : condition_model_input,
+            'negative_text': negative_condition_model_input
         }
         batch['mask'] = batch['mask'].type(torch.float32)
         
@@ -92,13 +102,19 @@ class Kandinsky3InpaintingPipeline:
         batch['text']['t5']['input_ids'] = batch['text']['t5']['input_ids'].unsqueeze(0).to(self.device)
         batch['text']['t5']['attention_mask'] = batch['text']['t5']['attention_mask'].unsqueeze(0).to(self.device)
         batch['mask'] = batch['mask'].unsqueeze(0).to(self.device)
+
+        if negative_condition_model_input is not None:
+            batch['negative_text']['t5']['input_ids'] = batch['negative_text']['t5']['input_ids'].unsqueeze(0).to(self.device)
+            batch['negative_text']['t5']['attention_mask'] = batch['negative_text']['t5']['attention_mask'].unsqueeze(0).to(self.device)
+
         return batch
         
     def __call__(
         self, 
-        text: str, 
+        text: str,
         image: PIL.Image.Image,
         mask: np.ndarray,
+        negative_text: str = None,
         images_num: int = 1, 
         bs: int = 1, 
         steps: int = 50,
@@ -106,11 +122,17 @@ class Kandinsky3InpaintingPipeline:
     ) -> List[PIL.Image.Image]:
     
         with torch.no_grad():
-            batch = self.prepare_batch(text, image, mask)
+            batch = self.prepare_batch(text, negative_text, image, mask)
             processed = self.shared_step(batch)
         
-        context_mask = processed['context_mask'].repeat_interleave(bs, dim=0)
-        context = processed['context'].repeat_interleave(bs, dim=0)
+        bs_context_mask = processed['context_mask'].repeat_interleave(bs, dim=0)
+        bs_context = processed['context'].repeat_interleave(bs, dim=0)
+
+        if processed['negative_context'] is not None:
+            bs_negative_context_mask = processed['negative_context_mask'].repeat_interleave(bs, dim=0)
+            bs_negative_context = processed['negative_context'].repeat_interleave(bs, dim=0)
+        else:
+            bs_negative_context, bs_negative_context_mask = None, None
 
         mask = processed['mask'].repeat_interleave(bs, dim=0) 
         masked_latent = processed['masked_latent'].repeat_interleave(bs, dim=0)
@@ -119,17 +141,22 @@ class Kandinsky3InpaintingPipeline:
         
         betas = get_named_beta_schedule('cosine', 50)
         base_diffusion = BaseDiffusion(betas, percentile=0.95)
-        base_diffusion.jump_length = 0
-        
+
+        pil_images = []    
         with torch.cuda.amp.autocast(enabled=self.fp16):
             with torch.no_grad():
                 images = base_diffusion.p_sample_loop(
                     self.unet, (bs, 4, masked_latent.shape[2], masked_latent.shape[3]), self.device, 
-                    context, context_mask, self.null_embedding, guidance_weight_text, 
+                    bs_context, bs_context_mask, self.null_embedding, guidance_weight_text, 
+                    negative_context=bs_negative_context, negative_context_mask=bs_negative_context_mask,
                     mask=mask, masked_latent=masked_latent, 
                 )
 
                 images = self.movq.decode(images)
             
         images = torch.clip((images + 1.) / 2., 0., 1.).cpu()
-        return self.to_pil(images[0])
+
+        for images_chunk in images.chunk(1):
+            pil_images += [self.to_pil(image) for image in images_chunk]
+
+        return pil_images
