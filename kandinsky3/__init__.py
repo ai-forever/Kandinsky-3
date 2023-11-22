688cd21986159e77f8672f68bcca8cbe1897426d
oriBetelgeuse <arkhipkin.v98@gmail.com>
2023-11-22 04:05:56 +0300

    add weights loading from hf

--- kandinsky3/__init__.py
+++ kandinsky3/__init__.py
@@ -4,6 +4,7 @@ import os
 
 import numpy as np
 from tqdm import tqdm
+from huggingface_hub import hf_hub_download
 
 import torch
 import omegaconf
@@ -22,7 +23,7 @@ def get_T2I_unet(
     device: Union[str, torch.device],
     weights_path: Optional[str] = None, 
     fp16: bool = False
-) -> (UNet, Optional[dict], Optional[torch.Tensor]):
+) -> (UNet, Optional[torch.Tensor], Optional[dict]):
     unet = UNet(
         model_channels=384,
         num_channels=4,
@@ -43,25 +44,10 @@ def get_T2I_unet(
     null_embedding = None
     projections_state_dict = None
     if weights_path:
-        ema_state_dict = {}
-        for file_name in tqdm(os.listdir(weights_path)):
-            ema_state_dict.update(torch.load(
-                os.path.join(weights_path, file_name), 
-                map_location=torch.device('cpu')
-            ))
-
-        projections_state_dict = {
-            key.replace('encoder.projections.', ''): value
-            for key, value in ema_state_dict.items()  if 'projections' in key
-        }
-        null_embedding = {
-            key.replace('null_embedding.', ''): value
-            for key, value in ema_state_dict.items()  if 'null_embedding' in key
-        }['null_embedding']
-        unet.load_state_dict({
-            key.replace('unet.', ''): value
-            for key, value in ema_state_dict.items()  if 'unet' in key
-        })
+        state_dict = torch.load(weights_path, map_location=torch.device('cpu'))
+        projections_state_dict = state_dict['projections']
+        null_embedding = state_dict['null_embedding']
+        unet.load_state_dict(state_dict['unet'])
     
     unet.eval().to(device)
 
@@ -78,7 +64,7 @@ def get_T5encoder(
     fp16: bool = True,
     low_cpu_mem_usage: bool = True,
     device_map: Optional[str] = None
-) -> (UNet, Optional[dict], Optional[torch.Tensor]):
+) -> (T5TextConditionProcessor, T5TextConditionEncoder):
     model_names = {'t5': weights_path}
     tokens_length = {'t5': 128}
     context_dim = 4096
@@ -99,7 +85,7 @@ def get_movq(
     device: Union[str, torch.device],
     weights_path: str, 
     fp16: bool = False
-) -> (UNet, Optional[dict], Optional[torch.Tensor]):
+) -> MoVQ:
     generator_config = {
         'double_z': False,
         'z_channels': 4,
@@ -125,9 +111,9 @@ def get_inpainting_unet(
     device: Union[str, torch.device],
     weights_path: Optional[str] = None, 
     fp16: bool = False
-) -> (UNet, Optional[dict], Optional[torch.Tensor]):
+) -> (UNet, Optional[torch.Tensor], Optional[dict]):
 
-    inpaint_unet = UNet(
+    unet = UNet(
         model_channels=384,
         num_channels=9,
         init_channels=192,
@@ -147,34 +133,68 @@ def get_inpainting_unet(
     null_embedding = None
     projections_state_dict = None
     if weights_path:
-        ema_state_dict = {}
-
-        if os.path.isdir(weights_path):
-            for file_name in os.listdir(weights_path):
-                ema_state_dict.update(torch.load(os.path.join(weights_path, file_name), map_location='cpu'))
-        elif os.path.isfile(weights_path):
-            ema_state_dict = torch.load(weights_path, map_location='cpu')
-            if 'state_dict' in ema_state_dict:
-                ema_state_dict = ema_state_dict['state_dict']
-
-        projections_state_dict = {
-            key.replace('encoder.projections.', ''): value
-            for key, value in ema_state_dict.items()  if 'projections' in key
-        }
-        null_embedding = {
-            key.replace('null_embedding.', ''): value
-            for key, value in ema_state_dict.items()  if 'null_embedding' in key
-        }['null_embedding']
-        unet_state_dict = {
-            key.replace('unet.', ''): value
-            for key, value in ema_state_dict.items()  if 'unet' in key
-        }
-        
-        inpaint_unet.load_state_dict(unet_state_dict, strict=False)
+        state_dict = torch.load(weights_path, map_location=torch.device('cpu'))
+        projections_state_dict = state_dict['projections']
+        null_embedding = state_dict['null_embedding']
+        unet.load_state_dict(state_dict['unet'])
     
     if fp16:
-        inpaint_unet = inpaint_unet.half()
+        unet = unet.half()
     
-    inpaint_unet.eval().to(device)
+    unet.eval().to(device)
 
-    return inpaint_unet, null_embedding, projections_state_dict
+    return unet, null_embedding, projections_state_dict
+
+
+def get_T2I_pipeline(
+    device: Union[str, torch.device],
+    fp16: bool = False,
+    cache_dir: str = '/tmp/kandinsky3/',
+    unet_path: str = None,
+    text_encode_path: str = None,
+    movq_path: str = None,
+) -> Kandinsky3T2IPipeline:
+    if unet_path is None:
+        unet_path = hf_hub_download(
+            repo_id="ai-forever/Kandinsky3.0", filename='weights/kandinsky3.pt', cache_dir=cache_dir
+        )
+    if text_encode_path is None:
+        text_encode_path = 'google/flan-ul2'
+    if movq_path is None:
+        movq_path = hf_hub_download(
+            repo_id="ai-forever/Kandinsky3.0", filename='weights/movq.pt', cache_dir=cache_dir
+        )
+
+    unet, null_embedding, projections_state_dict = get_T2I_unet(device, unet_path, fp16=fp16)
+    processor, condition_encoders = get_T5encoder(device, text_encode_path, projections_state_dict, fp16=fp16)
+    movq = get_movq(device, movq_path, fp16=fp16)
+    return Kandinsky3T2IPipeline(
+        device, unet, null_embedding, processor, condition_encoders, movq, fp16=fp16
+    )
+
+
+def get_inpainting_pipeline(
+        device: Union[str, torch.device],
+        fp16: bool = False,
+        cache_dir: str = '/tmp/kandinsky3/',
+        unet_path: str = None,
+        text_encode_path: str = None,
+        movq_path: str = None,
+) -> Kandinsky3InpaintingPipeline:
+    if unet_path is None:
+        unet_path = hf_hub_download(
+            repo_id="ai-forever/Kandinsky3.0", filename='weights/kandinsky3_inpainting.pt', cache_dir=cache_dir
+        )
+    if text_encode_path is None:
+        text_encode_path = 'google/flan-ul2'
+    if movq_path is None:
+        movq_path = hf_hub_download(
+            repo_id="ai-forever/Kandinsky3.0", filename='weights/movq.pt', cache_dir=cache_dir
+        )
+
+    unet, null_embedding, projections_state_dict = get_inpainting_unet(device, unet_path, fp16=fp16)
+    processor, condition_encoders = get_T5encoder(device, text_encode_path, projections_state_dict, fp16=fp16)
+    movq = get_movq(device, movq_path, fp16=fp16)
+    return Kandinsky3InpaintingPipeline(
+        device, unet, null_embedding, processor, condition_encoders, movq, fp16=fp16
+    )
